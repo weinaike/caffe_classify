9bf1d696dc511cd985ec7841473eff0e4a82dd18
weinaike <weinaike@163.com>
2016-07-25 14:38:51 +0800

    增加预处理

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
-#include <QCoreApplication>
 #include <opencv.hpp>
 #include <classifier.h>
 #include <ctrain.h>
 #include <gflags/gflags.h>
 #include <string>
 #include <cdirfile.h>
+#include <fstream>
 using namespace cv;
 using namespace std;
 void prep_treat(Mat & src,Mat & dst);
 void class_one()
 {
-    string path="/home/joyoung/qtwork/caffe_classify/demo-class5-google/";
+    string path="/home/joyoung/qtwork/caffe_classify/soymilk18-finetune/";
     string model_file=path+"deploy.prototxt";
     string mean_file=path+"mean.binaryproto";
-    string trained_file=path+"snapshot_iter.caffemodel";
+    string trained_file=path+"snapshot_iter_1k.caffemodel";
     string label_file=path+"labels.txt";
     Classifier classifier(model_file, trained_file, mean_file, label_file);
 
     VideoCapture cap;
     cap.open(0);
-    //Mat src,dst,src_org,dst_org,mean;
     Mat src,dst;
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT,1200);
-    cap.set(CV_CAP_PROP_FRAME_WIDTH,1600);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT,1600);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,1200);
+    Mat mean,dst2;
+    int mean_num=1;
     while(1)
     {
         double t=(double)getTickCount();
         cap>>src;
+
         prep_treat(src,dst);
+        /*
+        for(int i=0;i<mean_num;i++)
+        {
+            cap>>src;
+            prep_treat(src,dst);
+            dst.convertTo(dst2,CV_32FC3,1.0/255.0);
+            if(i==0)
+                mean=dst2/mean_num;
+            else
+                mean+=dst2/mean_num;
+        }
+        mean.convertTo(dst,CV_8UC3,255.0);
+        */
         std::vector<Prediction> predictions = classifier.Classify(dst,1);
         t=((double)getTickCount()-t)/getTickFrequency();
         Prediction p;
@@ -37,15 +52,16 @@ void class_one()
                     << p.first << "\"" <<"     time: "<<t<<"s"<<std::endl;
         }
         putText(dst,p.first,Point(20,30),FONT_HERSHEY_SIMPLEX,1,Scalar(255,255,255),2);
-        imshow("bean",dst);
-        waitKey(1);
 
+        imshow("test",dst);
+        waitKey(100);
     }
 
 }
 void prep_treat(Mat & src, Mat & dst)
 {
     Size m_size=src.size();
+    //GaussianBlur(src,src,Size(5,5),1.5);
     int len=0;
     if(m_size.height>m_size.width)
         len=m_size.width;
@@ -56,8 +72,10 @@ void prep_treat(Mat & src, Mat & dst)
     top=(m_size.height-len)/2;
     Rect m_rect;
     m_rect=Rect(left,top,len,len);
-    dst=src(m_rect);
-    cv::resize(dst,dst,Size(256,256));
+    Mat temp=src(m_rect);
+    cv::resize(temp,temp,Size(256,256));
+    Rect ROI=Rect(6,6,224,224);
+    dst=temp(ROI);
 }
 
 void train_test()
@@ -69,47 +87,119 @@ void train_test()
 }
 void class_many()
 {
-    string path="/home/joyoung/qtwork/caffe_classify/demo-class5-google/";
+    string path="/home/joyoung/qtwork/caffe_classify/soymilk18-google2/";
     string model_file=path+"deploy.prototxt";
     string mean_file=path+"mean.binaryproto";
-    string trained_file=path+"snapshot_iter.caffemodel";
+    string trained_file=path+"snapshot_iter_2k.caffemodel";
     string label_file=path+"labels.txt";
     Classifier classifier(model_file, trained_file, mean_file, label_file);
+    ifstream fin(label_file.c_str());
 
-    char *to_search="/home/joyoung/digits/data/class5-dealed/val/dami/";
-    CDirfile dirfile(to_search);
-    string filename;
+    string test_dir="/home/joyoung/digits/data/soymilk_test/";
 
-    Mat src,dst;
-    while(1)
+    string result_file=path+"result2k.txt";
+    ofstream outfile(result_file.c_str());//save file of result
+
+    vector<string> label_vec;
+    char label[200]={0};
+    while(fin.getline(label,sizeof(label)))
     {
-        filename=dirfile.getContent();
-        if(filename.length()==0) break;
-        src=imread(filename.c_str());
-        cout << filename << endl;
-        double t=(double)getTickCount();
-        prep_treat(src,dst);
-        std::vector<Prediction> predictions = classifier.Classify(dst,1);
-        t=((double)getTickCount()-t)/getTickFrequency();
-        Prediction p;
-        for (size_t i = 0; i < predictions.size(); ++i) {
-          p = predictions[i];
-          std::cout << std::fixed << std::setprecision(4) << p.second << " - \""
-                    << p.first << "\"" <<"     time: "<<t<<"s"<<std::endl;
+        label_vec.push_back(label);
+    }
+    int result[18][19]={0};// save result of predict, result[0] the max of image; result[1] the mount of correct predict
+    float percent[18];
+    for(int num_label=0;num_label<label_vec.size();num_label++)
+    {
+        int abc=0;
+
+        string img_dir=test_dir+label_vec[num_label]+".txt";
+        ifstream fin_image(img_dir.c_str());
+
+        //cout<<img_dir<<endl;
+        //CDirfile dirfile(img_dir.c_str());
+        string filename;
+        Mat src,dst;
+        char im_file[200]={0};
+        while(fin_image.getline(im_file,sizeof(im_file)))
+        {
+
+            filename=test_dir+im_file;
+            //string filename_save=test_dir+"256_256/"+im_file;
+            src=imread(filename.c_str());            
+            double t=(double)getTickCount();
+            prep_treat(src,dst);
+            //imwrite(filename_save,dst);
+            std::vector<Prediction> predictions = classifier.Classify(dst,1);
+            t=((double)getTickCount()-t)/getTickFrequency();
+
+            result[num_label][18]++;
+            Prediction p;
+            for (size_t i = 0; i < predictions.size(); ++i) {
+                p = predictions[i];
+                cout<<"src: "<<label_vec[num_label]<<"  predict: "<< p.first<<"  "<<std::fixed << std::setprecision(4)
+                       << p.second <<"   time: "<<t<<"s"<<std::endl;
+
+                if(label_vec[num_label]==p.first)
+                    result[num_label][num_label]++;
+                else
+                    for(int ss=0;ss<label_vec.size();ss++)
+                    {
+                        if(label_vec[ss]==p.first)
+                        {
+                            result[num_label][ss]++;
+                            //cout<<label_vec[num_label]<<"   "<<p.first<<"    "<<num_label<<" "<<ss<<endl;
+                        }
+                    }
+            }
+            abc++;
+            //if(abc>10)break;
+
         }
-        putText(dst,p.first,Point(20,30),FONT_HERSHEY_SIMPLEX,1,Scalar(255,255,255),2);
-        imshow("bean",dst);
-        waitKey(0);
+        percent[num_label]=result[num_label][num_label]/(result[num_label][18]+0.00001);
+
+        cout<<"percent:"<<percent[num_label]<<"      ";
+        cout<<"Max:"<<result[num_label][18]<<"        "<<"Yes:"<<result[num_label][num_label]
+              <<"       "<<label_vec[num_label]<<endl;
+
     }
 
+    for (int j=0;j<18;j++)
+    {
+        for (int i=0;i<18;i++)
+        {
+            cout.width(5);
+            cout<<result[j][i];
+            outfile.width(5);
+            outfile<<result[j][i];
+        }
+        cout<<endl;
+        outfile<<endl;
+    }
+    float sum=0;
+    for (int j=0;j<18;j++)
+    {
+        sum+=percent[j];
+        cout<<result[j][j]/(result[j][18]+0.00001);
+        cout<<"     Max: "<<result[j][18];
+        cout<<"     Yes: "<<result[j][j];
+        cout<<"     "<<label_vec[j]<<endl;
+
+        outfile<<result[j][j]/(result[j][18]+0.00001);
+        outfile<<"      Max: "<<result[j][18];
+        outfile<<"      Yes: "<<result[j][j];
+        outfile<<"      "<<label_vec[j]<<endl;
+    }
+
+    outfile<<"the mean percent : " <<sum/18<<endl;
+    cout<<"predict is over."<<"  the mean percent:"<<sum/18<<endl;
+    outfile.close();
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
     //class_one();
     class_many();
     //train_test();
-    return a.exec();
+    return(0);
 }
 
